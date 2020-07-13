76c224c5ed4deb9c9c51a6cca11659d135c9a67b
Zaenab <zfathima@akamai.com>
2020-07-13 12:56:34 +0530

    Removed args for chatops rule

--- sensors/sensor2.py
+++ sensors/sensor2.py
@@ -15,10 +15,7 @@ class ChatopsSensor(Sensor):
     def run(self):
         while not self._stop:
             self._logger.debug('HelloSensor dispatching trigger...')
-            count = self.sensor_service.get_value('hello_st2.count') or 0
-            payload = {'greeting': 'Chatops in action!', 'count': int(count) + 1}
-            self.sensor_service.dispatch(trigger='hello_st2.event1', payload=payload)
-            self.sensor_service.set_value('hello_st2.count', payload['count'])
+            self.sensor_service.dispatch(trigger='hello_st2.event2')
             eventlet.sleep(60)
 
     def cleanup(self):
