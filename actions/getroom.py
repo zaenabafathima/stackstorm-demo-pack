a9dd5caaf513b89fcba9dd9c460890ae41e32ee4
Zaenab <zfathima@akamai.com>
2020-07-13 13:18:36 +0530

    chatops action modified

--- actions/getroom.py
+++ actions/getroom.py
@@ -1,5 +1,14 @@
 
-class GetRoomChatops:
-    def run():
-        return (True, {'message': 'This works!!!'})
-    
+import sys
+
+from st2common.runners.base_action import Action
+
+class GetRoomChatops(Action):
+    def run(self):
+        message = 'working'
+        print(message)
+
+        if message == 'working':
+            return (True, message)
+        return (False, message)
+ 
