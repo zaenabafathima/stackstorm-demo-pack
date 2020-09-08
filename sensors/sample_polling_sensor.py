b87a05b5edf860307add012655ecf586c4f7a946
Zaenab <zfathima@akamai.com>
2020-09-08 15:52:10 +0530

    Polling sensor

--- sensors/sample_polling_sensor.py
+++ sensors/sample_polling_sensor.py
@@ -6,19 +6,20 @@ from st2reactor.sensor.base import PollingSensor
 class WorkingSensor(PollingSensor):
     def __init__(self, sensor_service, config, poll_interval=60):
         super(WorkingSensor, self).__init__(sensor_service=sensor_service, config=config)
+        self._poll_interval = poll_interval
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
         self._stop = False
-        # self._endpoint = 'https://zeroday-onboard.default.abattery.appbattery.nss1.tn.akamai.com/zeroday/v1/integration'
+        self._endpoint = 'https://zeroday-onboard.default.abattery.appbattery.nss1.tn.akamai.com/zeroday/v1/integration'
 
     def setup(self):
         pass
 
     def poll(self):
         self._logger.debug('WorkingSensor dispatching trigger...')
-        api_response = requests.get('http://www.google.com')
+        api_response = requests.get(self._endpoint, verify=False)
         payload = {
             'greeting': 'API Polling Working!',
-            'response': api_response.text
+            'response': api_response.json()
         }
         self.sensor_service.dispatch(trigger='hello_st2.eventX', payload=payload)
 
