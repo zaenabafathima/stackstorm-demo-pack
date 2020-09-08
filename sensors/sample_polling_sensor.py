8f1e6e638322df77ce305b03ccb4c9fbc1832ce7
Zaenab <zfathima@akamai.com>
2020-09-08 15:59:44 +0530

    Polling sensor

--- sensors/sample_polling_sensor.py
+++ sensors/sample_polling_sensor.py
@@ -3,24 +3,28 @@ import requests
 from st2reactor.sensor.base import PollingSensor
 
 
-class WorkingSensor(PollingSensor):
-    def __init__(self, sensor_service, config, poll_interval=60):
-        super(WorkingSensor, self).__init__(sensor_service=sensor_service, config=config)
+class ApiPollingSensor(PollingSensor):
+    def __init__(self, sensor_service, config, poll_interval=60, endpoint=''):
+        super(ApiPollingSensor, self).__init__(sensor_service=sensor_service, config=config)
         self._poll_interval = poll_interval
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
         self._stop = False
-        self._endpoint = 'https://zeroday-onboard.default.abattery.appbattery.nss1.tn.akamai.com/zeroday/v1/integration'
+        self._endpoint = endpoint or 'https://zeroday-onboard.default.abattery.appbattery.nss1.tn.akamai.com/zeroday/v1/integration'
 
     def setup(self):
         pass
 
     def poll(self):
         self._logger.debug('WorkingSensor dispatching trigger...')
-        api_response = requests.get(self._endpoint, verify=False)
         payload = {
             'greeting': 'API Polling Working!',
-            'response': api_response.json()
         }
+        try:
+            api_response = requests.get(self._endpoint, verify=False)
+            payload['response'] = api_response.json()
+        except Exception as e:
+            payload['response'] = 'An error occured!' + str(e)
+
         self.sensor_service.dispatch(trigger='hello_st2.integration_property_fetch', payload=payload)
 
     def cleanup(self):
