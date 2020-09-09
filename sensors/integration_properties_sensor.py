201770e6ffaecb5eaa74c3bd51e61e861a2b11a9
Zaenab <zfathima@akamai.com>
2020-09-09 11:21:00 +0530

    Polling sensor

--- sensors/integration_properties_sensor.py
+++ sensors/integration_properties_sensor.py
@@ -13,13 +13,47 @@ from sensors.sample_polling_sensor import ApiPollingSensor
 from st2reactor.sensor.base import PollingSensor
 
 
-class IntegrationPropertiesSensor(ApiPollingSensor, PollingSensor):
-    def __init__(self, sensor_service, config, poll_interval=60, endpoint=''):
+class IntegrationPropertiesSensor(PollingSensor):
+    def __init__(self, sensor_service, config, poll_interval=10, endpoint=''):
         super(IntegrationPropertiesSensor, self).__init__(
             sensor_service=sensor_service,
             config=config,
-            poll_interval=poll_interval,
-            endpoint='http://www.google.com',
-            trigger='hello_st2.integration_property_fetch',
-            greeting='Integration Property Sensor works!'
+            # poll_interval=poll_interval,
+            # endpoint='http://www.google.com',
+            # trigger='hello_st2.integration_property_fetch',
+            # greeting='Integration Property Sensor works!'
         )
+        self._poll_interval = poll_interval
+
+    def poll(self):
+        self.sensor_service.dispatch(trigger='hello_st2.integration_property_fetch', payload={'greeting': 'Integration greeting'})
+
+    def setup(self):
+        """
+        Run the sensor initialization / setup code (if any).
+        """
+        pass
+
+    def cleanup(self):
+        """
+        Run the sensor cleanup code (if any).
+        """
+        pass
+
+    def add_trigger(self, trigger):
+        """
+        Runs when trigger is created
+        """
+        pass
+
+    def update_trigger(self, trigger):
+        """
+        Runs when trigger is updated
+        """
+        pass
+
+    def remove_trigger(self, trigger):
+        """
+        Runs when trigger is deleted
+        """
+        pass
