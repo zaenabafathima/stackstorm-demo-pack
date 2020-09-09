7cf26e1c58aa0e0563265642328377a712443c6b
Zaenab <zfathima@akamai.com>
2020-09-09 15:49:38 +0530

    GSSBIZED-3929: Initial Commit

--- sensors/integration_properties_sensor.py
+++ sensors/integration_properties_sensor.py
@@ -1,17 +1,10 @@
-import json
-import requests
-import io
 import logging
-import time
-from typing import Union
-from urllib import parse
 
 
 LOGGER = logging.getLogger(__name__)
 
 from st2reactor.sensor.base import PollingSensor
 from base_sensors.base_poll_sensor import ApiPollingSensorBase
-
 
 
 class ApiPollingSensor2(ApiPollingSensorBase):
@@ -29,67 +22,37 @@ class ApiPollingSensor2(ApiPollingSensorBase):
         super().__init__(sensor_service=sensor_service, config=config,
                          endpoint='http://www.google.com',
                          trigger='hello_st2.event1',
-                         greeting='1 - STILL WORKING, YAY!!')
+                         greeting='2 - STILL WORKING, YAY!!')
         self._poll_interval = poll_interval
-        # self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
-        # self._endpoint = 'https://zeroday-onboard.default.abattery.appbattery.nss1.tn.akamai.com/zeroday/v1/integration'
-        # self._trigger = trigger or 'hello_st2.integration_property_fetch'
-        # self._greeting = greeting
 
-    # def poll(self):
-    #     # self._logger.debug('WorkingSensor dispatching trigger...')
-    #     payload = {
-    #         'greeting': self._greeting or 'DUPLICATE API Polling Sensor',
-    #         'status': None,
-    #         'response': None
-    #     }
-    #     try:
-    #         from base_sensors.base_poll_sensor import ApiPollingSensorBase
-    #     except Exception as e:
-    #         payload['response'] = str(e)
-    #     else:
-    #         payload['response'] = '2 - Imported the base!!' + str(ApiPollingSensorBase)
-    #     self.sensor_service.dispatch(trigger='hello_st2.integration_property_fetch', payload=payload)
-    #     # try:
-    #     #     api_response = requests.get(self._endpoint, verify=False)
-    #     #     payload['status'] = api_response.status_code
-    #     #     api_response.raise_for_status()
-    #     #     payload['response'] = api_response.json()
-    #     # except requests.exceptions.RequestException as err:
-    #     #     payload['response'] = str(err)
-    #     # except json.decoder.JSONDecodeError as json_err:
-    #     #     payload['response'] = 'JSON Decode Error! ' + str(json_err)
+    # def setup(self):
+    #     """
+    #     Run the sensor initialization / setup code (if any).
+    #     """
+    #     pass
 
-    #     # self.sensor_service.dispatch(trigger='hello_st2.integration_property_fetch', payload=payload)
+    # def cleanup(self):
+    #     """
+    #     Run the sensor cleanup code (if any).
+    #     """
+    #     pass
 
-    def setup(self):
-        """
-        Run the sensor initialization / setup code (if any).
-        """
-        pass
+    # def add_trigger(self, trigger):
+    #     """
+    #     Runs when trigger is created
+    #     """
+    #     pass
 
-    def cleanup(self):
-        """
-        Run the sensor cleanup code (if any).
-        """
-        pass
+    # def update_trigger(self, trigger):
+    #     """
+    #     Runs when trigger is updated
+    #     """
+    #     pass
 
-    def add_trigger(self, trigger):
-        """
-        Runs when trigger is created
-        """
-        pass
-
-    def update_trigger(self, trigger):
-        """
-        Runs when trigger is updated
-        """
-        pass
-
-    def remove_trigger(self, trigger):
-        """
-        Runs when trigger is deleted
-        """
-        pass
+    # def remove_trigger(self, trigger):
+    #     """
+    #     Runs when trigger is deleted
+    #     """
+    #     pass
 
     
