5b715e1dab4661b2ebac3b90d6ef403b74099a16
Zaenab <zfathima@akamai.com>
2020-09-09 09:40:33 +0530

    Polling sensor

---
+++ sensors/integration_properties_sensor.py
@@ -0,0 +1,24 @@
+import json
+import requests
+import io
+import logging
+import time
+from typing import Union
+from urllib import parse
+
+
+LOGGER = logging.getLogger(__name__)
+
+from sample_polling_sensor import ApiPollingSensor
+
+
+class IntegrationPropertiesSensor(ApiPollingSensor):
+    def __init__(self, sensor_service, config, poll_interval=60, endpoint=''):
+        super(IntegrationPropertiesSensor, self).__init__(
+            sensor_service=sensor_service,
+            config=config,
+            poll_interval=poll_interval,
+            endpoint='http://www.google.com',
+            trigger='hello_st2.integration_property_fetch',
+            greeting='Integration Property Sensor works!'
+        )
