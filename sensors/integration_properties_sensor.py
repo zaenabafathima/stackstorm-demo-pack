cfb00dca83434884f62e208b17f424e702416580
Zaenab <zfathima@akamai.com>
2020-09-11 11:08:54 +0530

    Tests

--- sensors/integration_properties_sensor.py
+++ sensors/integration_properties_sensor.py
@@ -23,5 +23,5 @@ class IntegrationPropertiesSensor(ApiPollingSensorBase):
         super().__init__(sensor_service=sensor_service, config=config,
                          endpoint='https://zeroday-onboard.default.abattery.appbattery.nss1.tn.akamai.com/zeroday/v1/integration',
                          trigger='hello_st2.integration_property_fetch',
-                         greeting='9 - STILL WORKING, YAY!!')
+                         greeting='10 - STILL WORKING, YAY!!')
         self._poll_interval = poll_interval
