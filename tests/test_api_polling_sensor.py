bc8167afc1006cdc9b923c21970447d003a6548a
Zaenab <zfathima@akamai.com>
2020-09-11 11:37:34 +0530

    Tests

--- tests/test_api_polling_sensor.py
+++ tests/test_api_polling_sensor.py
@@ -1,4 +1,5 @@
 import unittest
+import requests
 # from stackstorm_demo_pack.sensors.base_sensors import ApiPollingSensor
 from integration_properties_sensor import IntegrationPropertiesSensor
 from base_api import ApiPollingSensorBase
@@ -7,7 +8,7 @@ from st2tests.mocks.sensor import MockSensorService
 
 
 class MySensorSensorTestCase(BaseSensorTestCase):
-    sensor_cls = IntegrationPropertiesSensor
+    sensor_cls = ApiPollingSensorBase
 
     def test_method(self):
         print("Testing!!!!")
@@ -21,3 +22,16 @@ class MySensorSensorTestCase(BaseSensorTestCase):
         print("Make request method:", sensor.make_request_with_retry)
         print("Sensor poll time:", sensor._poll_interval)
         # ...
+
+    def test_make_request_with_retry(self):
+        sensor = self.get_sensor_instance(
+            config={
+                'foo': 'bar',
+                'poll_interval': 28
+            })
+        print("Testing make request with retry")
+        try:
+            response = sensor.make_request_with_retry(url='localhost:8000/alias/', method='GET', headers={})
+        except Exception as e:
+            print("Error while hitting localhost:", str(e))
+
