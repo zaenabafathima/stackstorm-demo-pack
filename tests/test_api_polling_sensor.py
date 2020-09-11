ecf98ddb086957f30be524cc0a4cabe9f062e196
Zaenab <zfathima@akamai.com>
2020-09-11 15:43:08 +0530

    Tests

--- tests/test_api_polling_sensor.py
+++ tests/test_api_polling_sensor.py
@@ -1,10 +1,17 @@
 import unittest
 import requests
-# from stackstorm_demo_pack.sensors.base_sensors import ApiPollingSensor
-from integration_properties_sensor import IntegrationPropertiesSensor
-from base_api import ApiPollingSensorBase
+
+from unittest import mock
+
+from base_api_polling_sensor import ApiPollingSensorBase
 from st2tests.base import BaseSensorTestCase
 from st2tests.mocks.sensor import MockSensorService
+
+
+class MockResponse:
+    def __init__(self, json_data, status_code):
+        self.content = json_data
+        self.status_code = status_code
 
 
 class MySensorSensorTestCase(BaseSensorTestCase):
@@ -12,26 +19,45 @@ class MySensorSensorTestCase(BaseSensorTestCase):
 
     def test_method(self):
         print("Testing!!!!")
-        sensor = self.get_sensor_instance(
-            config={
-                'foo': 'bar',
-                'poll_interval': 28
-            })
+        sensor = self.get_sensor_instance(config={'foo': 'bar'})
         sensor.poll()
         self.assertEqual(len(self.get_dispatched_triggers()), 1)
         print("Make request method:", sensor.make_request_with_retry)
         print("Sensor poll time:", sensor._poll_interval)
         # ...
 
-    def test_make_request_with_retry(self):
-        sensor = self.get_sensor_instance(
-            config={
-                'foo': 'bar',
-                'poll_interval': 28
-            })
-        print("Testing make request with retry")
-        try:
-            response = sensor.make_request_with_retry(url='localhost:8000/alias/', method='GET', headers={})
-        except Exception as e:
-            print("Error while hitting localhost:", str(e))
-
+    def test_get_request_with_retry(self):
+        """Something."""
+        sensor = self.get_sensor_instance(config={'foo': 'bar'})
+        retry_count = 3
+        requests.request = mock.Mock()
+        requests.request.return_value = MockResponse({}, 500)
+        sensor.make_request_with_retry(
+            url='http://someurl.com/test.json',
+            method='GET',
+            retry_wait_time=5,
+            retry_count=retry_count,
+            headers={},
+        )
+        print("Mocked request called:", requests.request.call_count)
+        self.assertEqual(requests.request.call_count, retry_count)
+        requests.request.return_value = MockResponse({}, 400)
+        sensor.make_request_with_retry(
+            url='http://someurl.com/test.json',
+            method='GET',
+            retry_wait_time=5,
+            retry_count=retry_count,
+            headers={},
+        )
+        print("Mocked request called:", requests.request.call_count)
+        self.assertEqual(requests.request.call_count, 1)
+        requests.request.return_value = MockResponse({}, 200)
+        sensor.make_request_with_retry(
+            url='http://someurl.com/test.json',
+            method='GET',
+            retry_wait_time=5,
+            retry_count=retry_count,
+            headers={},
+        )
+        print("Mocked request called:", requests.request.call_count)
+        self.assertEqual(requests.request.call_count, 1)
