5dc23a4d33cf4b6a488a64f5a962b1f4c14b6ad0
Zaenab <zfathima@akamai.com>
2020-09-14 11:52:10 +0530

    test

--- sensors/base_api_polling_sensor.py
+++ sensors/base_api_polling_sensor.py
@@ -44,23 +44,23 @@ class ApiPollingSensorBase(PollingSensor):
             'endpoint': self._endpoint,
             'is_valid_json': True
         }
-        # try:
-        #     response = self.make_request_with_retry(url=self._endpoint,
-        #                                             headers=self._headers,
-        #                                             params=self._params)
-        # except requests.exceptions.RequestException as err:
-        #     payload['response'] = str(err)
-        # except Exception as e:
-        #     payload['response'] = str(e)
-        # else:
-        #     payload['status_code'] = response.status_code
-        #     try:
-        #         payload['response'] = response.json()
-        #     except json.decoder.JSONDecodeError as err:
-        #         payload['response'] = str(err)
-        #         payload['is_valid_json'] = False
-        #     except Exception as e:
-        #         payload['response'] = str(e)
+        try:
+            response = self.make_request_with_retry(url=self._endpoint,
+                                                    headers=self._headers,
+                                                    params=self._params)
+        except requests.exceptions.RequestException as err:
+            payload['response'] = str(err)
+        except Exception as e:
+            payload['response'] = str(e)
+        else:
+            payload['status_code'] = response.status_code
+            try:
+                payload['response'] = response.json()
+            except json.decoder.JSONDecodeError as err:
+                payload['response'] = str(err)
+                payload['is_valid_json'] = False
+            except Exception as e:
+                payload['response'] = str(e)
         self.sensor_service.dispatch(trigger=self._trigger, payload=payload)
 
     def setup(self):
@@ -84,18 +84,18 @@ class ApiPollingSensorBase(PollingSensor):
         pass
 
     def make_request_with_retry(
-            self,
-            url: str,
-            headers: dict,
-            method: str = 'GET',
-            data: Union[dict, str] = None,
-            params: dict = None,
-            retry_count: int = None,
-            retry_wait_time: int = None,
-            verify: str = False,
-            cert: tuple = None,
-            timeout: tuple = None,
-            extra_logging_args: dict = None,
+        self,
+        url: str,
+        headers: dict,
+        method: str = "GET",
+        data: Union[dict, str] = None,
+        params: dict = None,
+        retry_count: int = None,
+        retry_wait_time: int = None,
+        verify: str = False,
+        cert: tuple = None,
+        timeout: tuple = None,
+        extra_logging_args: dict = None,
     ):
         """
         Retry a HTTP request with a configured retry count and timeout.
@@ -123,20 +123,16 @@ class ApiPollingSensorBase(PollingSensor):
                 read_timeout = 6000
                 timeout = (6000, read_timeout)
 
-            err_msg = (
-                "Request Failed with status code {status_code}, method: {method}, url: {url}, "
-                "headers: {headers}, params: {params}, data: {data}, verify: {verify}, "
-                "cert: {cert}, response: {response}"
-            )
-            report_id = None
-            api_struct = None
+            err_msg = "Request Failed with status code {status_code},"\
+                      "method: {method}, url: {url}, "\
+                      "headers: {headers}, params: {params}, data: {data},"\
+                      "verify: {verify}, "\
+                      "cert: {cert}, response: {response}"
             response = None
 
             if extra_logging_args is not None and isinstance(
-                    extra_logging_args, dict
+                extra_logging_args, dict
             ):
-                report_id = extra_logging_args.get("report_id")
-                api_struct = extra_logging_args.get("api_structure")
                 extra_msg = ", "
                 for _key, _val in extra_logging_args.items():
                     if isinstance(_val, dict):
@@ -146,7 +142,6 @@ class ApiPollingSensorBase(PollingSensor):
                 err_msg += extra_msg
 
             start_time = time.time()
-            parsed_uri = parse.urlparse(url)
             cur_retry_wait_time = retry_wait_time
 
             request_args = {
@@ -164,22 +159,17 @@ class ApiPollingSensorBase(PollingSensor):
                 response = requests.request(**request_args)
                 LOGGER.info("%s", response.status_code)
             except (
-                    requests.exceptions.HTTPError,
-                    requests.exceptions.ConnectionError,
-                    requests.exceptions.ConnectTimeout,
-                    requests.exceptions.ChunkedEncodingError,
+                requests.exceptions.HTTPError,
+                requests.exceptions.ConnectionError,
+                requests.exceptions.ConnectTimeout,
+                requests.exceptions.ChunkedEncodingError,
             ) as exc:
-                print(
-                    "cannot complete request due to {} for report_id {} api structure {}".format(
-                        str(exc),
-                        report_id,
-                        api_struct,
-                    ))
+                print("cannot complete request due to {0}".format(str(exc)))
                 request_exception_occurred = True
 
             while retry_count > 0 and (
-                    request_exception_occurred
-                    or (response is not None and response.status_code >= 500)
+                request_exception_occurred
+                or (response is not None and response.status_code >= 500)
             ):
                 if response is not None:
                     LOGGER.error(
@@ -192,25 +182,20 @@ class ApiPollingSensorBase(PollingSensor):
                             data=data,
                             verify=verify,
                             cert=cert,
-                            response=response.content,
+                            response=response.content
                         )
                     )
                 print(
-                    "sleeping for {0} seconds, report_id {1} api structure {2} retry count={3}"
-                    .format(
-                        cur_retry_wait_time,
-                        report_id,
-                        api_struct,
-                        retry_count
-                    ))
-                retry_count -= 1
+                    "sleeping for {0} seconds, retry count={1}".format(
+                        cur_retry_wait_time, retry_count
+                    )
+                )
                 time.sleep(cur_retry_wait_time)
                 LOGGER.info(
-                    "making request now for report_id {} api structure {}, remaining retry counts {}".format(
-                        report_id,
-                        api_struct,
-                        retry_count - 1,
-                    ))
+                    "making request now, remaining retry counts {0}".format(
+                        retry_count
+                    )
+                )
                 start_time = time.time()
                 try:
                     response = requests.request(**request_args)
@@ -229,11 +214,10 @@ class ApiPollingSensorBase(PollingSensor):
                 )
             if response.status_code in [200, 201]:
                 LOGGER.info(
-                    "Total time taken for api structure {0} report_id {1} is {2} seconds".format(
-                        api_struct,
-                        report_id,
+                    "Total time taken for api is {0} seconds".format(
                         int(time.time() - start_time),
-                    ))
+                    )
+                )
             elif response.status_code < 500:
                 LOGGER.error(
                     err_msg.format(
@@ -248,11 +232,15 @@ class ApiPollingSensorBase(PollingSensor):
                         response=response.content,
                     )
                 )
-            print("Returning response with status code:", response.status_code)
             return response
         except Exception as exc:
-            if type(exc).__name__ != requests.exceptions.ConnectionError.__name__:
+            if (
+                type(exc).__name__
+                != requests.exceptions.ConnectionError.__name__
+            ):
 
-                LOGGER.exception("Unhandled exception in make_request_with_retry")
+                LOGGER.exception(
+                    "Unhandled exception in make_request_with_retry"
+                )
             raise
 
